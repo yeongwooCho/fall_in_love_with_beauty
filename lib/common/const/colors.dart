785583578d6932c9a340a1ba2617d758063bb492
yeongwoo <jyy0223@naver.com>
2024-06-01 16:17:39 +0900

    [feat] default color 추가

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  // Main
+  static const primary = Color(0xFFE77D7D);
+  static const secondary = Color(0xFFC45E65);
+  static const third = Color(0xFF25596E);
+
+  // Text Color
+  static const text = Color(0xFF1C1C1E);
+  static const white = Color(0xFFFDFDFD);
+  static const error = Color(0xFFFF555F);
+  static const notice = Color(0xFFEBF4FF);
+
+  static const darkGrey = Color(0xFF767676);
+  static const middleGrey = Color(0xFFE5E5EA);
+  static const lightGrey = Color(0xFFF4F5F9);
+  static const barrier = Color(0x3F1C1C1E);
+  static const empty = Color(0x00FFFFFF);
+  static const shadow = Color(0x3F000000);
+  static const heart = Color(0xFFFA6230);
+}
