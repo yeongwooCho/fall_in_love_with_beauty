ebb5d498599a864d7829a767cb0f52f08e5db5f9
yeongwoo <jyy0223@naver.com>
2024-06-01 16:17:57 +0900

    [feat] default image path 추가

---
+++ lib/common/const/image_path.dart
@@ -0,0 +1,32 @@
+class ImagePath {
+  /// logo
+  static const appIcon = "asset/img/logo/app_icon.png";
+  static const logoWhite = "asset/img/logo/logo_white.png";
+  static const logoBlack = "asset/img/logo/logo_black.png";
+  static const serviceImage = "asset/img/logo/service.png";
+
+  /// concept
+  static const concept = "asset/img/concept/concept.png";
+  static const circulation = "asset/img/concept/circulation.png";
+  static const manufacturing = "asset/img/concept/manufacturing.png";
+  static const map = "asset/img/concept/map.png";
+
+  /// social_login button
+  static const appleIcon = "asset/img/social_login/button/apple.png";
+  static const googleIcon = "asset/img/social_login/button/google.png";
+  static const kakaoIcon = "asset/img/social_login/button/kakao.png";
+  static const naverIcon = "asset/img/social_login/button/naver.png";
+
+  /// social_login screen
+  static const appleScreen = "asset/img/social_login/screen/apple.png";
+  static const googleScreen = "asset/img/social_login/screen/google.png";
+  static const kakaoScreen = "asset/img/social_login/screen/kakao.png";
+  static const naverScreen = "asset/img/social_login/screen/naver.png";
+
+  /// user
+  static const user = "asset/img/user/user.png";
+
+  /// banner
+  static const banner = "asset/img/banner/banner.png";
+  static const bannerBand = "asset/img/banner/banner_band.png";
+}
